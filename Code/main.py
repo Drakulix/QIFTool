87a0e5de290f45a53b4b8242eb2cba865b64dcf9
bloombert-fu <robert.selack@gmx.de>
2020-08-11 17:56:48 +0200

    working on statscodefrequency

--- Code/main.py
+++ Code/main.py
@@ -17,9 +17,23 @@ def testhub():
 
 
 def search():
-    repo = auth.search_repositories(query='youtube+music')
-    for r in repo:
-        print(r)
+    repo = auth.search_repositories(query='ytmdesktop')
+    #repo = auth.search_repositories(query='youtube')
+    repo0 = repo[0]
+    compurl = repo0.compare('master@{all}', 'master')
+    scf = repo0.get_stats_code_frequency()
+
+    diff = compurl.get__repr__(compurl.raw_data)
+    scf1 = scf[0].deletions
+    additions = 0
+    deletions = 0
+    for add_del in scf:
+        additions += add_del.additions
+        deletions += add_del.deletions
+        print(add_del.additions, add_del.deletions, add_del.week, '\n')
+    print(additions, deletions)
+
+    print(repo0, scf1, len(scf), diff)
 
 
 # Press the green button in the gutter to run the script.
