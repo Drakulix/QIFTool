80cecc3f0ca02f1b2bd3797ead8aa41f12b2f53f
bloombert-fu <robert.selack@gmx.de>
2020-08-21 19:09:38 +0200

    added sqlite database

--- Code/main.py
+++ Code/main.py
@@ -7,6 +7,8 @@ installed packages: apt install cURL
 import urllib.request
 import json
 import time
+import sqlite3
+from sqlite3 import Error
 from github import Github
 import Metric_StatsCodeFrequency  # metric file for StatsCodeFrequency
 import Metric_Contributors  # metric file for Contributors
@@ -19,6 +21,79 @@ auth = Github("c2c1d13983cbb8c1d9ce7845c20d7937ba7c25a0", per_page=1000)
 
 file = open('database.txt', 'a')
 
+
+# ----------------------------------------------------------------------------------------------- #
+
+def create_connection(db_file):
+    """
+    create a database connection to the SQLite database specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+    return conn
+
+
+def create_table(conn, create_table_statement):
+    """
+    create the table for all the repositories
+    :param create_table_statement: create table statement
+    :param conn: connection object
+    :return: False
+    """
+    try:
+        cursor = conn.cursor()
+        cursor.execute(create_table_statement)
+    except Error as e:
+        print(e)
+
+
+def create_database():
+    """
+    creates the databse file and tables
+    :return: False
+    """
+    # choose path for the database to be created at or none if you want the database to be created in
+    # the current directory
+    database = r"/home/robert/OSCTool/Code/OSCTool.db"
+    repository_table_statement = """ CREATE TABLE IF NOT EXISTS repositories (
+                                        repo_id integer NOT NULL,
+                                        repo_name text NOT NULL,
+                                        repo_creator_name text NOT NULL,
+                                        code_frequency integer,
+                                        commits text,
+                                        contributors integer,
+                                        issues text
+                                        ); """
+    # create the database connection
+    conn = create_connection(database)
+
+    # create the repositories table if it does not exist yet
+    if conn is not None:
+        create_table(conn, repository_table_statement)
+    else:
+        print('Error! Cannot create database connection.')
+
+
+def test_input():
+    try:
+        database = r"/home/robert/OSCTool/Code/OSCTool.db"
+        conn = create_connection(database)
+        cursor = conn.cursor()
+        with conn:
+            input_test = (1, 'testrepo', 'testcreator', 999, 'refactor', 100, 'new')
+            insert_statement = """INSERT INTO repositories VALUES (?, ?, ?, ?, ?, ?, ?) """
+            cursor.execute(insert_statement, input_test)
+    except Error as e:
+        print(e)
+
+
+# ------------------------------------------------------------------------------------------------ #
 
 def testhub():
     repo = auth.get_repo("PyGithub/PyGithub")
@@ -78,4 +153,6 @@ def query():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    query()
+    create_database()
+    test_input()
+    # query()
